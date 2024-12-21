bbb39038022276d6f10355e101e609ce805370ae
crazysadboy <54902146+CrazyFatBoi2005@users.noreply.github.com>
2024-12-21 23:19:43 +0300

    orders adding and deleting

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app.secret_key = 'SECRETKEY'
 
 def admin_required(func):
     from functools import wraps
+
     @wraps(func)
     def decorated_view(*args, **kwargs):
         if session.get('role_id') != 0:
@@ -18,7 +19,6 @@ def admin_required(func):
     return decorated_view
 
 
-# Создание базы данных и таблиц
 def init_db():
     with sqlite3.connect("database.db") as conn:
         cursor = conn.cursor()
@@ -59,14 +59,13 @@ def init_db():
                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                        user_id INTEGER NOT NULL,
                        type_id INTEGER NOT NULL,
+                       phone TEXT NOT NULL DEFAULT '+77777777777',
                        comment TEXT,
                        files TEXT,
                        FOREIGN KEY (user_id) REFERENCES users(id)
                        FOREIGN KEY (type_id) REFERENCES work_types(id)
                    )
                ''')
-
-        # Добавляем тестовые данные (если пусто)
         cursor.execute("SELECT COUNT(*) FROM users")
         if cursor.fetchone()[0] == 0:
             cursor.executemany('''
@@ -81,7 +80,6 @@ def init_db():
 
 @app.route('/')
 def home():
-    print(session)
     return render_template('index.html')
 
 
@@ -100,10 +98,9 @@ def get_session_data():
 @app.route('/register', methods=['POST'])
 def register():
     if request.method == 'POST':
-        data = request.get_json()  # Получаем JSON-данные из тела запроса
+        data = request.get_json()
         if not data:
             return jsonify({"message": "No data provided"}), 400
-
         username = data.get('username')
         email = data.get('email')
         password = data.get('password')
@@ -111,7 +108,6 @@ def register():
         if not username or not email or not password:
             return jsonify({"message": "Please provide all fields"}), 400
 
-        # Проверка на существование пользователя с таким именем
         with sqlite3.connect("database.db") as conn:
             cursor = conn.cursor()
             cursor.execute('SELECT id FROM users WHERE username = ?', (username,))
@@ -120,24 +116,16 @@ def register():
             if existing_user:
                 return jsonify({"message": "Username already exists"}), 400
 
-            # Хэширование пароля перед сохранением
             hashed_password = generate_password_hash(password)
-
-            # Вставка нового пользователя в базу данных
             cursor.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
                            (username, email, hashed_password))
             conn.commit()
-
-            # Получаем id нового пользователя
             cursor.execute('SELECT id, role_id FROM users WHERE username = ?', (username,))
             user = cursor.fetchone()
-
-            # Добавление данных пользователя в сессию
             session.clear()
             session['user_id'] = user[0]
             session['username'] = username
-            session['role_id'] = user[1]  # Можно добавить роль или другие данные
-
+            session['role_id'] = user[1]
             return jsonify({"message": "Registration successful", "success": True}), 200
 
     return jsonify({"message": "Method not allowed"}), 405
@@ -146,7 +134,7 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        data = request.get_json()  # Получаем JSON-данные из тела запроса
+        data = request.get_json()
         if not data:
             return jsonify({"success": False, "message": "Данные не были переданы."}), 400
 
@@ -197,31 +185,20 @@ def add_order():
 
     if not phone or not category or not url:
         return jsonify({"message": "Пожалуйста, заполните все обязательные поля"}), 400
-
     try:
-        # Соединяемся с базой данных
         with sqlite3.connect("database.db") as conn:
             cursor = conn.cursor()
-
-            # Получаем тип услуги по категории
             cursor.execute('''SELECT id FROM work_types WHERE title = ?''', (category,))
             type_id = cursor.fetchone()
-
             if not type_id:
                 return jsonify({"message": "Категория услуги не найдена"}), 400
-
             type_id = type_id[0]
-
-            # Добавляем заказ в таблицу orders
             cursor.execute('''
-                INSERT INTO orders (user_id, type_id, comment, files)
-                VALUES (?, ?, ?, ?)
-            ''', (1, type_id, comments, url))  # user_id=1 для примера
-
+                INSERT INTO orders (user_id, type_id, phone, comment, files)
+                VALUES (?, ?, ?, ?, ?)
+            ''', (session.get('user_id'), type_id, phone, comments, url))
             conn.commit()
-
         return jsonify({"message": "Заказ успешно добавлен!"}), 200
-
     except Exception as e:
         return jsonify({"message": f"Ошибка при добавлении заказа: {str(e)}"}), 500
 
@@ -229,7 +206,42 @@ def add_order():
 @app.route('/admin')
 @admin_required
 def admin():
-    return render_template('admin.html')
+    with sqlite3.connect('database.db') as conn:
+        sort_by = request.args.get('sort_by', default=None)
+        cursor = conn.cursor()
+
+        if sort_by == 'type':
+            cursor.execute('''
+                            SELECT orders.id, orders.user_id, orders.phone, orders.comment, orders.files, work_types.title AS category
+                            FROM orders
+                            JOIN work_types ON orders.type_id = work_types.id ORDER BY orders.type_id
+                        ''')
+            orders = cursor.fetchall()
+        elif sort_by == 'id':
+            cursor.execute('''
+                            SELECT orders.id, orders.user_id, orders.phone, orders.comment, orders.files, work_types.title AS category
+                            FROM orders
+                            JOIN work_types ON orders.type_id = work_types.id ORDER BY orders.id
+                        ''')
+            orders = cursor.fetchall()
+        else:
+            cursor.execute('''
+                            SELECT orders.id, orders.user_id, orders.phone, orders.comment, orders.files, work_types.title AS category
+                            FROM orders
+                            JOIN work_types ON orders.type_id = work_types.id
+                        ''')
+            orders = cursor.fetchall()
+    return render_template('admin.html', orders=orders)
+
+
+@app.route('/admin/delete/<int:order_id>', methods=['POST'])
+@admin_required
+def delete_order(order_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM orders WHERE id = ?", (order_id,))
+        conn.commit()
+    return redirect(url_for('admin'))
 
 
 if __name__ == '__main__':
